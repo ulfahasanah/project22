7cc5449d0c6da594d2ef703bf99ac85b7c5dff46
ulfa hasanah <ulfa2912@outlook.com>
2022-03-03 17:33:01 +0700

    feat(jwt): add jwt

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Employee;
 
 class EmployeeController extends Controller
 {
-    //
+    public function getEmployee()
+    {
+        $employee = Employee::get();
+        return response()->json($employee);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,16 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('login', 'AuthController@login');
+Route::post('register', 'AuthController@register');
+
+Route::group([
+    'middleware' => 'auth.jwt'
+], function () {
+
+    Route::get('/employee', 'EmployeeController@getEmployee');
+    Route::post('/logout', 'AuthController@logout');
+    Route::post('/refresh', 'AuthController@refresh');
+    Route::get('/user-profile', 'AuthController@userProfile');
+
 });
